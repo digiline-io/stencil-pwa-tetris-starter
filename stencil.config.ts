749731b02fb1ba32c4f060a6765e205f22ac46a8
Gino Bernardi <ginobernardi@att.net>
2020-12-10 18:12:08 -0500

    init commit

---
+++ stencil.config.ts
@@ -0,0 +1,26 @@
+import { Config } from '@stencil/core';
+import { readFileSync } from 'fs';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  outputTargets: [
+    {
+      type: 'www',
+      // comment the following line to disable service workers in production
+      serviceWorker: null,
+      baseUrl: 'https://myapp.local/'
+    }
+  ],
+  devServer: {
+    reloadStrategy: 'hmr',
+    openBrowser: false,
+    port: 3333,
+    https: {
+      cert: readFileSync('local.cert', 'utf8'),
+      key: readFileSync('local.key', 'utf8')
+    }
+  }
+};
